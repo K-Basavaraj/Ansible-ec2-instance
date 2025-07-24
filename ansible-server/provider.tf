fb856e5678397de7588f6d04e6992dd957770d89
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-07-24 23:49:34 +0530

    deploy test1

--- ansible-server/provider.tf
+++ ansible-server/provider.tf
@@ -12,10 +12,3 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
-
-  # backend "s3" {
-  #   bucket = "remotes3-ansible"
-  #   key    = "ansible-server"
-  #   region = "us-east-1"
-  #   dynamodb_table = "k8slocks3"
-  # }
