9bc9450e4396ce6199aa9a6527cce1b36d65f402
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-07-25 11:04:02 +0530

    done terraform fmt

--- ansible-server/main.tf
+++ ansible-server/main.tf
@@ -4,10 +4,10 @@ module "ansible-server" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
   # convert StringList to list and get first element
-  subnet_id                      = var.public_subnet_id
-  ami                            = data.aws_ami.ami_info.id
-  user_data                      = file("ansible.sh")
-  user_data_replace_on_change    = true 
+  subnet_id                   = var.public_subnet_id
+  ami                         = data.aws_ami.ami_info.id
+  user_data                   = file("ansible.sh")
+  user_data_replace_on_change = true
   tags = {
     Name = "workstation"
   }
